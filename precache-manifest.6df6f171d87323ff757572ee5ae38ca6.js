799a826cd816312fb4ac6cc0a8513cc595c185d0
Viri Ortiz <viriortiz32@gmail.com>
2021-07-10 02:43:54 -0500

    Updates

---
+++ precache-manifest.6df6f171d87323ff757572ee5ae38ca6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "befde2d65e782cc41bd25ff3eaeae5d1",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "097ab278394714515543",
+    "url": "/react-portfolio/static/css/main.2612de57.chunk.css"
+  },
+  {
+    "revision": "240e413da0035c3716f9",
+    "url": "/react-portfolio/static/js/2.29234ef1.chunk.js"
+  },
+  {
+    "revision": "097ab278394714515543",
+    "url": "/react-portfolio/static/js/main.a6118538.chunk.js"
+  },
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  },
+  {
+    "revision": "19cd62f9c168c16847e5e9355e303b79",
+    "url": "/react-portfolio/static/media/bg1.19cd62f9.jpg"
+  }
+]);
